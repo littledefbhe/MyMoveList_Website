aafd6fb1acf00f9b751a75e049b622b976d740ac
EJ <ej.dcruz657@gmail.com>
2025-11-12 18:46:55 +1100

    Moved the folders backwards so I could upload to python anywhere easier

--- MyMovieList/app.py
+++
@@ -1,7 +0,0 @@
-from board import create_app
-
-# Create the Flask application using the factory function
-app = create_app()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
